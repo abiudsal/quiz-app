5a911b361e8236ca3ece1dca20f6b27b72a0b75e
Abiud Salazar <abiud.salazar@alumno.buap.mx>
2021-10-17 10:42:45 -0500

    Rediseño de Categorías y se añade clase correct a respuestas

--- src/app/pages/quiz/quiz.component.ts
+++ src/app/pages/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FileService } from '../../helpers/file.service';
 import { Quiz } from '../../models/Quiz';
@@ -9,14 +9,25 @@ import { Quiz } from '../../models/Quiz';
   styleUrls: ['./quiz.component.scss']
 })
 export class QuizComponent implements OnInit {
+  @ViewChild("answer0") answer0!: ElementRef;
+  @ViewChild("answer1") answer1!: ElementRef;
+  @ViewChild("answer2") answer2!: ElementRef;
+  @ViewChild("answer3") answer3!: ElementRef;
+  @ViewChild("answer4") answer4!: ElementRef;
+
+
+
   public quiz:Quiz
   public nQuestion: number;
   public nAllQuestions: number;
+
+
+
   //public id: string
   constructor(
     private fileService: FileService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router    
   ) { 
     let id = this.route.snapshot.paramMap.get('id'); 
     if(id===null){
@@ -30,9 +41,21 @@ export class QuizComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    //this.formFocus.nativeElement.focus();
+  }
+
   nextQuestion(){
     if(this.nQuestion < this.quiz.questions.length)
       this.nQuestion++
+  }
+
+  checkAnswer(i: number){
+    if(i===this.quiz.questions[this.nQuestion].index){
+      //alert("fdsafdsa "+i)
+      this.answer0.nativeElement.classList.add('correct');
+    }
+
   }
 
 }
