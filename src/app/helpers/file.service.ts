fdf75ddc253d1d274716e1c3b58dece3668cf614
Abiud Salazar <abiud.salazar@alumno.buap.mx>
2021-10-17 19:11:49 -0500

    Se puso en funcionamiento la página de Categorías. Los elementos de las subcategorías redireccionan a su respectivo quiz

--- src/app/helpers/file.service.ts
+++ src/app/helpers/file.service.ts
@@ -6,6 +6,7 @@ import { Quiz } from '../models/Quiz';
   providedIn: 'root'
 })
 export class FileService {
+    public categories = ["Geografía", "Historia Universal", "Español", "Matemáticas", "Arte"];
     public quiz = [
         new Quiz("Capitales", "Geografía",[
             new Question("¿Cuál es la capital de Argentina?", [
@@ -24,11 +25,71 @@ export class FileService {
                 "Egipto","El Cairo", "Guiza", "Nilo"
             ], 1),   
         ]),
-        new Quiz("Hola mundo", "Aprendiendo",[
-            new Question("¿Cuantos años tienes?", [
-                "Si","No", "Tal vez", "mmm bueno"
-            ], 2)   
+        new Quiz("Continentes", "Geografía",[
+            new Question("¿En qué continente está España?", [
+                "Africa","América", "Asia", "Europa" 
+            ], 3),
+            new Question("¿En qué continente está Nigeria?", [
+                "Africa","América", "Asia", "Europa" 
+            ], 0),   
+            new Question("¿En qué continente está Venezuela?", [
+                "Africa","América", "Asia", "Europa" 
+            ], 1),   
+            new Question("¿En qué continente está Nueva Zelanda?", [
+                "Africa","Oceanía", "Asia", "Europa" 
+            ], 1),   
+            new Question("¿En qué continente está Tailandia?", [
+                "Africa","América", "Asia", "Europa" 
+            ], 2),   
+            new Question("¿En qué continente está Cuba?", [
+                "Africa","Oceanía", "Asia", "América" 
+            ], 3),   
+            new Question("¿En qué continente está Australia?", [
+                "Africa","Oceanía", "Asia", "Europa" 
+            ], 1),   
         ]),
+        new Quiz("Segunda Guerra Mundial", "Historia Universal",[
+            new Question("¿En qué año inició la Segunda Guerra Mundial?", [
+                "1937","1938", "1939", "1940"
+            ], 2),
+            new Question("¿Cuál fue el acontecimiento que dió inició a la Segunda Guerra Mundial?", [                
+                "Alemania invadió Polonia", "Francia invadió Polonia", "Rusia invadió Polonia"
+            ], 0),
+            new Question("¿Cuál de lo siguientes países NO perteneció al Eje?", [                
+                "Alemania", "Italia", "URSS", "Japón"
+            ], 2),
+            new Question("¿Cuál de lo siguientes países NO perteneció a los Aliados?", [                
+                "Estados Unidos", "Italia", "URSS", "Gran Bretaña"
+            ], 1),
+            new Question("¿Cuál fue el acontecimiento que provocó que Estados Unidos se unierá a los Aliados?", [                
+                "El inicio de la Guerra Fría", "Alemania invadió Francia", "El ataque por parte de Japón a la base de Pearl Harbor"
+            ], 0),
+            new Question("¿En qué año terminó la Segunda Guerra Mundial?", [
+                "1944","1945", "1946", "1947"
+            ], 1),            
+            new Question("¿Cuál fue el número estimado de bajas durante la Segunda Guerra Mundial?", [                
+                "Entre 20 y 30 millones", "Entre 30 y 40 millones", "Entre 50 y 60 millones", "Más de 100 millones"
+            ], 2),
+              
+        ]),
+        new Quiz("Primera Guerra Mundial", "Historia Universal",[
+            new Question("¿Cuál es la capital de Argentina?", [
+                "Buenos Aires","Argentina", "Tabasco", "Montevideo"
+            ], 0),
+            new Question("¿Cuál es la capital de Alemania?", [
+                "París","Berlín", "Moskú", "Praga"
+            ], 1),   
+            new Question("¿Cuál es la capital de México?", [
+                "México","Estado de México", "Tenochtitlan", "Ciudad de México"
+            ], 3),   
+            new Question("¿Cuál es la capital de Corea del Sur?", [
+                "Oslo","Estambul", "Tokio", "Seúl"
+            ], 3),   
+            new Question("¿Cuál es la capital de Egipto?", [
+                "Egipto","El Cairo", "Guiza", "Nilo"
+            ], 1),   
+        ]),
+        
     ]
 
     constructor() { 
@@ -42,5 +103,21 @@ export class FileService {
   
     getAll(){
         return [...this.quiz];
-      }
+    }
+    
+    getAllCategories(){
+        return [...this.categories];
+    }
+
+    getAllCategoriesSorted(){
+        let temp = [...this.categories];
+
+        temp = temp.sort((a:string, b:string) =>{
+            if(a < b) { return -1; }
+            if(a > b) { return 1; }
+            return 0;
+        })
+
+        return temp;
+    }
 }
--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FileService } from '../../helpers/file.service';
+import { Quiz } from '../../models/Quiz';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  public categories:Array<String> = [] 
+  public quizzes: Array<Quiz> = []
+
+  constructor(
+    private fileService: FileService
+  ) { 
+    this.categories = fileService.getAllCategoriesSorted()
+    this.quizzes = fileService.getAll()
+  }
 
   ngOnInit(): void {
   }
+
+
 
 }
